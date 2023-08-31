1ca2dc5c2c4bcaa3b55a476e82e3da948cbe826c
GustiDDI <143582498+GustiDDI@users.noreply.github.com>
2023-08-31 15:46:20 +0700

    data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,16 @@
+CREATE TABLE Family (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
 
+CREATE TABLE Person (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    family_id INT,
+    FOREIGN KEY (family_id) REFERENCES Family(id)
+);
+
+INSERT INTO Family (name) VALUES ('Smith');
+INSERT INTO Person (name, surname, family_id)
+VALUES ('John', 'Smith', (SELECT id FROM Family WHERE name = 'Smith'));
